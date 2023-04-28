2de78ffd2dd1fe285f9d8d17b45f81d2973c8322
Michelle Elias Flores <michelleeliasflores@gmail.com>
2023-04-27 20:08:19 -0400

    Initial Commit

---
+++ newmichelleapp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  newmichelleapp
+//
+//  Created by Michelle Elias Flores on 4/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ newmichelleapp/newmichelleappApp.swift
@@ -0,0 +1,17 @@
+//
+//  newmichelleappApp.swift
+//  newmichelleapp
+//
+//  Created by Michelle Elias Flores on 4/27/23.
+//
+
+import SwiftUI
+
+@main
+struct newmichelleappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
